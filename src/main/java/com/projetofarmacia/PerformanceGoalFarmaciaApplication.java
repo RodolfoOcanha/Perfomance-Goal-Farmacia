3b7a0463857677583c295477cb19df78da3ff094
RodolfoOcanha <rodolfo.ocanha@hotmail.com>
2024-03-26 10:26:03 -0300

    Perfomance Goal

---
+++ src/main/java/com/projetofarmacia/PerformanceGoalFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.projetofarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceGoalFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceGoalFarmaciaApplication.class, args);
+	}
+
+}
